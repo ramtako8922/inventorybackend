b29833261affd3472e50f5804177f630b03ed2b0
Ricardo <rmc892020@gmail.com>
2024-03-17 12:36:54 -0500

    inicio proyecto de inventario

---
+++ inventory/src/test/java/com/mejia/inventory/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mejia.inventory.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
